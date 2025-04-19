687add74465e15925eb4d54a5038aff19828aec6
fajarrijki27 <fajarrijkipaujan2007@gmail.com>
2025-04-19 12:43:38 +0700

    progres new

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,44 +1,46 @@
+<?php // file: template/sidebar.php
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+
 <div class="sidebar" id="sidebar">
-  <div class="sidebar-content">
-    <div class="header-title">PARK-IR 🚗</div>
+    <div class="sidebar-content">
+        <div class="header-title">PARK-IR 🚗</div>
 
-    <ul class="nav">
-      <li class="nav-item active">
-        <a href="dashboard.php">
-          <span class="indicator"></span>
-          <i class="fas fa-tachometer-alt"></i> Dashboard
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="users.php">
-          <span class="indicator"></span>
-          <i class="fas fa-users"></i> Users
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="kendaraan.php">
-          <span class="indicator"></span>
-          <i class="fas fa-car"></i> Kendaraan
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="blok_parkir.php">
-          <span class="indicator"></span>
-          <i class="fas fa-th-large"></i> Blok parkir
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="transaksi.php">
-          <span class="indicator"></span>
-          <i class="fas fa-money-bill"></i> Transaksi
-        </a>
-      </li>
-    </ul>
+        <ul class="nav">
+            <li class="nav-item <?= $activePage == 'dashboard' ? 'active' : '' ?>">
+                <a href="dashboard.php">
+                    <span class="indicator"></span>
+                    <i class="fas fa-tachometer-alt"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item <?= $activePage == 'users' ? 'active' : '' ?>">
+                <a href="users.php">
+                    <span class="indicator"></span>
+                    <i class="fas fa-users"></i> Users
+                </a>
+            </li>
+            <li class="nav-item <?= $activePage == 'kendaraan' ? 'active' : '' ?>">
+                <a href="kendaraan.php">
+                    <span class="indicator"></span>
+                    <i class="fas fa-car"></i> Kendaraan
+                </a>
+            </li>
+            <li class="nav-item <?= $activePage == 'blok_parkir' ? 'active' : '' ?>">
+                <a href="blok_parkir.php">
+                    <span class="indicator"></span>
+                    <i class="fas fa-th-large"></i> Blok parkir
+                </a>
+            </li>
+            <li class="nav-item <?= $activePage == 'transaksi' ? 'active' : '' ?>">
+                <a href="transaksi.php">
+                    <span class="indicator"></span>
+                    <i class="fas fa-money-bill"></i> Transaksi
+                </a>
+            </li>
+        </ul>
 
-    <div class="logout">
-      <a href="logout.php">
-        <i class="fas fa-sign-out-alt"></i> <span>Log out</span>
-      </a>
+        <div class="logout">
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i> <span>Log out</span></a>
+        </div>
     </div>
-  </div>
 </div>
